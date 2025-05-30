55a313c07b4fdce411c49604aa69571ca80a7b7b
jbhoon01@naver.com <jbhoon01@naver.com>
2025-05-31 02:54:54 +0900

    Feat : 영화 조회 class 추가

--- DB_request.py
+++ DB_request.py
@@ -4,6 +4,7 @@ import json
 from datasets import load_dataset
 import pandas as pd
 import numpy as np
+import os
 #from analysis import emotion_analysis
 
 #책 관련 DB 조회 클래스
@@ -36,7 +37,6 @@ class book_db:
         max_val = idf.max()
         norm_idf = (idf - min_val) / (max_val - min_val)
         return norm_idf
-
     
 
     #하나의 감정을 입력하면 그에 따른 결과를 출력
@@ -45,8 +45,8 @@ class book_db:
         # 감정 단어가 tag에 포함된 행만 필터링
         filtered = self.data[self.data['tag'].str.contains(emotion, case=False, na=False)]
         # 만약 평점 컬럼이 있으면, 높은 순으로 정렬
-        #if 'avg_rating' in filtered.columns:
-        #    filtered = filtered.sort_values('avg_rating', ascending=False)
+        if 'avg_rating' in filtered.columns:
+            filtered = filtered.sort_values('avg_rating', ascending=False)
         # 상위 top_n개만 추출
         filtered = filtered.head(top_n)[['item_id', 'tag', 'avg_rating']]
         # 메타데이터(제목, 저자)와 병합
@@ -59,26 +59,103 @@ class book_db:
         norm_specificity = self.normalized_tag_specificity(self.data)
         result['norm_specificity'] = result['tag'].map(norm_specificity)
         #구체성 컬럼값 기준으로 내림차순 정렬
-        result = result.sort_values('norm_specificity', ascending=False).head(top_n).reset_index(drop=True)
+        #result = result.sort_values('norm_specificity', ascending=False).head(top_n).reset_index(drop=True)
 
         #top_n만 반환
-        return result#.head(top_n)
+        return result#.head(top_n)s
     
     #여러 감정들을 리스트로 받아서 전체 순회
-    def lookup_all(self, text):
-        emotions = text # text => 유저의 감정 분석 결과 
+    def lookup_all(self, text): 
+        emotions = text # text => 유저의 감정 분석 결과 리스트 // 수치값을 빼고 어떤 태그가 있는지만 사용
         result = []
         for emo in emotions:
             recom = self.recommend_books_by_emotion(emo)
             result.extend(recom)
         return result
 
-
-db = book_db()
-print(db.recommend_books_by_emotion('funny'))
+#db 조회 테스트용 코드
+#db = book_db()
+#print(db.recommend_books_by_emotion('funny'))
 
 class movie_db:
-    a = 0
+
+    global dataset
+    #global metadata
+
+    def __init__(self):
+        self.load_data()
+
+    def load_data(self):
+        #필요한 데이터 셋 직접 로드 // 현재는 3가지 데이터셋 로드중
+        self.movies = load_dataset(
+            "csv",
+            data_files={
+                "movies": "https://huggingface.co/datasets/lime1327/movie_data/resolve/main/archive/movie.csv"
+            },
+            split = "movies"
+        )
+        self.genome_tags = load_dataset(
+            "csv",
+            data_files={
+                "genome_tags": "https://huggingface.co/datasets/lime1327/movie_data/resolve/main/archive/genome_tags.csv"
+            },
+            split = "genome_tags"
+        )
+        self.genome_scores = load_dataset(
+            "csv",
+            data_files={
+                "genome_scores": "https://huggingface.co/datasets/lime1327/movie_data/resolve/main/archive/genome_scores.csv"
+            },
+            split = "genome_scores"
+        )
+  
+    #emotion 입력을 통해 해당하는 태그로 db조회후 추천 영화를 받음
+    def recommend_movies_by_emotion(self, emotion, top_n = 10):
+        #데이터셋 변환
+        movies = self.movies.to_pandas()
+        genome_tags = self.genome_tags.to_pandas()
+        genome_scores = self.genome_scores.to_pandas()
+
+        #감정에 해당하는 태그 ID 찾기
+        matched_tags = genome_tags[genome_tags['tag'].str.contains(emotion, case=False, na=False)]
+        if matched_tags.empty:
+            #입력한 태그와 일치하는 태그가 없다면.. -> 검색 불가 => 종료 or return -1 하고 따로 처리
+            exit()
+
+        # 여러 태그 중 relevance가 가장 높은 영화 추천 (상위 10개)
+        recommendations = pd.DataFrame()
+
+        for tag_id in matched_tags['tagId']:
+            # 해당 태그의 relevance가 높은 영화 100개 추출
+            tag_scores = genome_scores[genome_scores['tagId'] == tag_id]
+            top_movies = tag_scores.sort_values(by='relevance', ascending=False).head(100)
+            recommendations = pd.concat([recommendations, top_movies])
+
+        #여러 태그가 있다면 relevance 높은 것 우선으로 정렬
+        recommendations = recommendations.sort_values(by='relevance', ascending=False)
+
+        #영화 정보와 조인
+        recommendations = recommendations.merge(movies, on='movieId', how='left')
+        recommendations = recommendations.drop_duplicates(subset='movieId')
+
+        recom = recommendations[['title', 'relevance']].head(top_n).to_string(index=False)
+
+        return recom
+    
+    def lookup_all(self, text):
+        emotions = text # text => 유저의 감정 분석 결과 리스트 // 수치값을 빼고 어떤 태그가 있는지만 사용
+        result = []
+        for emo in emotions:
+            recom = self.recommend_movies_by_emotion(emo)
+            result.extend(recom)
+        return result
+
+#db 조회 테스트용 코드
+#db = movie_db()
+#rerom = db.recommend_movies_by_emotion('sad')
+#print(rerom)
+
+
 
 
 class music_db:
